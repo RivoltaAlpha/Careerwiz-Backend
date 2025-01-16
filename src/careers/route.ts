42dcbfaf4da9414f4ca7c542114c6b469d54e602
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-01-16 14:07:50 +0300

    career routes

---
+++ src/careers/route.ts
@@ -0,0 +1,10 @@
+import { Hono } from "hono";
+import { listCareers, getCareer, createCareer, updateCareer, deleteCareer } from "./controller";
+
+export const careersRouter = new Hono();
+
+careersRouter.get('/all-careers', listCareers);
+careersRouter.get('/get-career/:id', getCareer);
+careersRouter.post('/create-career', createCareer);
+careersRouter.put('/update-career', updateCareer);
+careersRouter.delete('/delete-career/:id', deleteCareer);
