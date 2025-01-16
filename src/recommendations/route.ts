a79450110b39740f28fedc0e4c3c014189eb3da4
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-01-16 14:14:40 +0300

    rename fix

--- src/recommendations/route.ts
+++ src/recommendations/route.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
-import { listRecommendations, getRecommendations, createRecommendations, updateRecommendations, getStudentRecommendations } from "./controller";
+import { listRecommendations,deleteRecommendations, getRecommendations, createRecommendations, updateRecommendations, getStudentRecommendations } from "./controller";
 
 export const recommendationsRouter = new Hono();
 
 recommendationsRouter.get('/all-recommendations', listRecommendations);
-recommendationsRouter.get('/get-student-recommendations/:id', getRecommendations);
-recommendationsRouter.post('/update-recommendations', createRecommendations);
-recommendationsRouter.put('/delete-recommendation/:id', updateRecommendations);
+recommendationsRouter.get('/get-recommendation/:id', getRecommendations);
+recommendationsRouter.post('/create-recommendations', createRecommendations);
+recommendationsRouter.put('/update-recommendation/:id', updateRecommendations);
+recommendationsRouter.delete('/delete-recommendation/:id', deleteRecommendations);
 recommendationsRouter.get('/get-student-recommendations/:id', getStudentRecommendations);
