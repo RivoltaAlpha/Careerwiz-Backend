78c6086b4c8ebfb8a9ac5e33d382ca697482c9bf
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-01-16 13:33:46 +0300

    Feedback services

---
+++ src/feedback/services.ts
@@ -0,0 +1,32 @@
+import db from "../drizzle/db";
+import { eq } from "drizzle-orm";
+import {TIFeedback,feedback,TSFeedback,} from "../drizzle/schema";
+
+
+export const AllfeedbackService = async ( limit?: number): Promise<TSFeedback[] | null> => {
+    if (limit) {
+        return await db.query.feedback.findMany({
+            limit: limit,
+        });
+    }
+    return await db.query.feedback.findMany();
+}
+
+export async function getFeedbackById(id: number): Promise<Array<TSFeedback>> {
+    return db.select().from(feedback).where(eq(feedback.feedback_id, id));
+}
+
+export async function createFeedbackService(data: TIFeedback) {
+    await db.insert(feedback).values(data);
+    return "Feedback created successfully";
+}
+
+export async function updateFeedbackService(id: number, feedbackData: TIFeedback) {
+    await db.update(feedback).set(feedbackData).where(eq(feedback.feedback_id, id));
+    return "Feedback updated successfully";
+}
+
+export async function deleteFeedbackService(id: number) {
+    await db.delete(feedback).where(eq(feedback.feedback_id, id));
+    return "Feedback deleted successfully";
+}
