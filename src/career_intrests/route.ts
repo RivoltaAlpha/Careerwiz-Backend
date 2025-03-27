28db37fb06f803f526431c9029dd6fb1a8314d81
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-03-27 16:30:12 +0300

    typo

--- src/career_intrests/route.ts
+++ src/career_intrests/route.ts
@@ -6,6 +6,6 @@ export const careerInterestsRouter = new Hono();
 careerInterestsRouter.get('/all-career-interests', listCareerInterests);
 careerInterestsRouter.get('/get-career-interest/:id', getCareerInterest);
 careerInterestsRouter.post('/create-career-interest', createCareerInterest);
-careerInterestsRouter.put('/update-career-intrest', updateCareerInterest);
+careerInterestsRouter.put('/update-career-intrest/:id', updateCareerInterest);
 careerInterestsRouter.delete('/delete-career-intrest/:id', deleteCareerInterest);
 careerInterestsRouter.get('/get-students-career-interests/:id', getingStudentCareerInterests);
